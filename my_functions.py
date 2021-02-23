f77d3f5e09f3719779de7e4b0b0afb5fbc60a2d3
michaelmccoll <mccollmichael@hotmail.com>
2021-02-23 18:52:29 +0000

    Initial homework week 1, day 2

---
+++ my_functions.py
@@ -0,0 +1,12 @@
+def greet(name, time_of_day):
+  return "Good " + time_of_day + ", " + name
+
+name_1 = "Bob"
+time_of_day_1 = "morning"
+greeting = greet(name_1, time_of_day_1)
+print(greeting)
+
+name_2 = "Ada"
+time_of_day_2 = "afternoon"
+greeting = greet(name_2, time_of_day_2)
+print(greeting)
